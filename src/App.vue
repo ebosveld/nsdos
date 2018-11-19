6f7fecb53282eb4532e969619d0d54e948867b90
Edwin Bosveld <edwin@inwy.nl>
2018-11-19 23:07:52 +0100

    Base styling and page layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>NS Reisplanner</header>
+
+	<footer>
+		<ul>
+			<li>
+				<span>F9</span> Zoek met vertrektijd
+			</li>
+			<li>
+				<span>F10</span> Zoek met aankomsttijd
+			</li>
+			<li>
+				<span>?</span> Help
+			</li>
+		</ul>
+	</footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
-  components: {
+  name: "app"
+  /*components: {
     HelloWorld
-  }
-}
+  }*/
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+body {
+  font-family: $font-family;
+  font-size: 1.51vw;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: $background;
+  color: $foreground;
+  margin: 0;
+  padding: 0;
+}
+
+header,
+footer {
+  width: 100%;
+  background: $menu-background;
+  color: $menu-foreground;
+  padding: 0.175em 0.375em;
+}
+
+header {
+  margin: 0;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+
+  ul,
+  li {
+    padding: 0;
+    display: inline;
+  }
+
+  li {
+    list-style-type: none;
+    span {
+      color: $menu-fnkey;
+    }
+    padding-right: 1em;
+  }
 }
 </style>
